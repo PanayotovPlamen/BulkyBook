27718b6302720daf3b0edc78b6501cdc261e9e8a
Plamen Stefanov Panayotov <panaiotowplamen@gmail.com>
2021-05-30 17:37:55 +0300

    Add project files.

---
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+            Category = new CategoryRepository(_context);
+            SP_Call = new SP_Call(_context);
+        }
+
+        public ICategoryRepository Category { get; private set; }
+        public ISP_Call SP_Call { get; private set; }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
